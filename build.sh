3f8ceefe9d58dac8eaac1b0d6dc8a1ab45bbb9ce
Michael Angstadt <mike.angstadt@gmail.com>
2011-06-12 12:51:09 -0400

    Added command for installing PEAR.
    Change-Id: I6e33601ab0e03a43682a0050ba515f3ba9c2d507

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #Install phar-util via PEAR (https://github.com/koto/phar-util)
+#sudo apt-get install php-pear
 #sudo pear channel-discover pear.kotowicz.net
 #sudo pear install kotowicz/PharUtil-beta
 
